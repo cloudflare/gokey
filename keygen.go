9954045ee411640428922a141aaef0fa0230a3ae
Danny Fast <dannyfast@gmail.com>
2022-09-20 11:03:01 -0700

    removing special special characters

--- keygen.go
+++ keygen.go
@@ -100,6 +100,7 @@ func (spec *PasswordSpec) Compliant(password string) bool {
 }
 
 const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" //  Excluding the ugly`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
+// const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
 
 func randRange(rng io.Reader, max byte) (byte, error) {
 	var base [1]byte
